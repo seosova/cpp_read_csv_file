71471fd2ab8b34b2eed7aa9adf4c8cf72db80596
Denis Savenko <seosova@gmail.com>
2015-05-29 00:59:36 +0300

    Show time!

---
+++ read_csv.cpp
@@ -0,0 +1,24 @@
+// read_csv.cpp: определяет точку входа для консольного приложения.
+// Программа для чтения csv файла. Отдельный работающий класс и пример использования.
+
+#include "stdafx.h"
+#include <Windows.h>
+#include <iostream>
+using namespace std;
+
+class CsvFile {
+protected:
+
+public:
+
+};
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    system("pause");
+	return 0;
+}
+
